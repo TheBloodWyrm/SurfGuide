c37af118f1d5e68be7f177d97023bca078389742
petpac12 <petpac12@new.edvhtl.at>
2015-08-17 21:43:46 +0200

    [17.08.15] Added Toolbar and DrawerLayout for MainActivity, added BuiltUpActivity with collapsing toolbar (still in progress)

--- app/src/main/java/geondan/surfguide/Activities/MainActivity.java
+++ app/src/main/java/geondan/surfguide/Activities/MainActivity.java
@@ -1,18 +1,34 @@
 package geondan.surfguide.Activities;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import geondan.surfguide.HomeMenu.HomeMenuAdapter;
 import geondan.surfguide.R;
 
-public class MainActivity extends Activity
-{
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle drawerToggle;
+
+    CoordinatorLayout rootLayout;
+
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdaper;
+    private HomeMenuAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
@@ -21,14 +37,97 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initToolbar();
+        initInstances();
+
         mRecyclerView = (RecyclerView) findViewById(R.id.home_menu_recycler_view);
         mRecyclerView.setHasFixedSize(true);
 
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        mAdaper = new HomeMenuAdapter();
-        mRecyclerView.setAdapter(mAdaper);
+        mAdapter = new HomeMenuAdapter();
+        mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
+
+
+        mAdapter.setOnItemClickListener(new HomeMenuAdapter.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(View v, int position)
+            {
+                if (position == 0)
+                {
+                    System.out.println("It Works for the Build Up");
+                    Intent i = new Intent(getApplicationContext(), BuiltUpActivity.class);
+                    startActivity(i);
+                } else if (position == 1)
+                {
+                    System.out.println("It Works for the Jibes");
+                } else if (position == 2)
+                {
+                    System.out.println("It Works for Tacks");
+                } else if (position == 3)
+                {
+                    System.out.println("It Works for Built Up 2");
+                }
+            }
+        });
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void initInstances() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        drawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, R.string.hello_world, R.string.hello_world);
+        drawerLayout.setDrawerListener(drawerToggle);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        rootLayout = (CoordinatorLayout) findViewById(R.id.rootLayout);
+
+    }
+
+    @Override
+    public void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item))
+            return true;
+
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
